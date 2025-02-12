0d08c13d8db2e0c8503901f844a85e45c4d492cf
Ongun <info@ongunakay.com>
2025-02-12 20:47:21 +0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ const Header = () => {
   if (!isClient) return null; // Avoid rendering on the server to prevent mismatch
 
   return (
-    <header className="bg-white shadow p-3 d-flex justify-content-between align-items-center">
+    <header className="bg-[#283c4a] p-3 d-flex justify-content-between align-items-center mx-auto w-100" style={{ fontFamily: "'Source Sans Pro', sans-serif", fontSize: "13px" }}>
       <div className="d-flex align-items-center gap-3">
         {/* Logo */}
         <div style={{ width: "40px", height: "40px", position: "relative" }}>
@@ -43,50 +43,50 @@ const Header = () => {
         className="d-lg-none btn btn-link"
         onClick={() => setIsMenuOpen(!isMenuOpen)}
       >
-        <FaBars size={24} />
+        <FaBars size={24} className="text-white" />
       </button>
 
       {/* Navigation */}
       <nav className={`d-flex flex-column flex-lg-row align-items-center gap-4 ${isMenuOpen ? "d-block" : "d-none"} d-lg-flex`}>
         <ul className="nav">
           <li className="nav-item text-center mx-2">
-            <a href="#" className="nav-link text-dark">
+            <a href="#" className="nav-link text-white">
               <FaHome className="fs-5" />
               <div>Home</div>
             </a>
           </li>
           <li className="nav-item text-center mx-2">
-            <a href="#" className="nav-link text-dark">
+            <a href="#" className="nav-link text-white">
               <FaUserFriends className="fs-5" />
               <div>My Network</div>
             </a>
           </li>
           <li className="nav-item text-center mx-2">
-            <a href="#" className="nav-link text-dark">
+            <a href="#" className="nav-link text-white">
               <FaSuitcase className="fs-5" />
               <div>Jobs</div>
             </a>
           </li>
           <li className="nav-item text-center mx-2">
-            <a href="#" className="nav-link text-dark">
+            <a href="#" className="nav-link text-white">
               <FaEnvelope className="fs-5" />
               <div>Messaging</div>
             </a>
           </li>
           <li className="nav-item text-center mx-2">
-            <a href="#" className="nav-link text-dark">
+            <a href="#" className="nav-link text-white">
               <FaBell className="fs-5" />
               <div>Notifications</div>
             </a>
           </li>
           <li className="nav-item text-center mx-2">
-            <a href="#" className="nav-link text-dark">
+            <a href="#" className="nav-link text-white">
               <Image src="/picture.jpeg" alt="Profile" width={30} height={30} className="rounded-circle" />
               <div>Me <FaCaretDown /></div>
             </a>
           </li>
           <li className="nav-item text-center mx-2">
-            <a href="#" className="nav-link text-dark">
+            <a href="#" className="nav-link text-white">
               <FaTh className="fs-5" />
               <div>Work <FaCaretDown /></div>
             </a>
